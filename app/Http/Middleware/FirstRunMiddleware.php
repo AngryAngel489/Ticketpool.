d1dcc58d8ea8eccbe3c62cabf86f4977607c6f50
Adrien Poupa <adrien@poupa.fr>
2018-12-10 15:34:20 -0500

    Update FirstRunMiddleware.php

--- app/Http/Middleware/FirstRunMiddleware.php
+++ app/Http/Middleware/FirstRunMiddleware.php
@@ -21,12 +21,12 @@ class FirstRunMiddleware
          * If there are no organisers then redirect the user to create one
          * else - if there's only one organiser bring the user straight there.
          */
-        $corganizerCount = Organiser::scope()->count();
-        if ($corganizerCount === 0 && !($request->route()->getName() === 'showCreateOrganiser') && !($request->route()->getName() === 'postCreateOrganiser')) {
+        $organizerCount = Organiser::scope()->count();
+        if ($organizerCount === 0 && !($request->route()->getName() === 'showCreateOrganiser') && !($request->route()->getName() === 'postCreateOrganiser')) {
             return redirect(route('showCreateOrganiser', [
                 'first_run' => '1',
             ]));
-        } elseif ($corganizerCount === 1 && ($request->route()->getName() === 'showSelectOrganiser')) {
+        } elseif ($organizerCount === 1 && ($request->route()->getName() === 'showSelectOrganiser')) {
             return redirect(route('showOrganiserDashboard', [
                 'organiser_id' => Organiser::scope()->first()->id,
             ]));
