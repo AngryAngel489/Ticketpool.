08d5f9e652e1333a420a97c4b44b92ad9d938855
Melvin Thoabala <melvin.t@superbalist.com>
2020-06-22 23:56:57 +0200

    Allow use of both reCaptcha hCaptcha or no capture based on config (#808)
    * WIP: add config for captcha and allow user to choose between them
    
    * add captha on sign up
    
    * clean up
    
    * read captcha switch from env

--- resources/views/Public/LoginAndRegister/Signup.blade.php
+++ resources/views/Public/LoginAndRegister/Signup.blade.php
@@ -74,7 +74,7 @@
                     </div>
                 </div>
                 @endif
-                @if(config('attendize.recaptcha_site'))
+                @if(config('attendize.captcha.recaptcha_is_on')  && config('attendize.recaptcha_site'))
                     <input id="captcha" type="hidden" name="grecaptcha">
                     <script src="https://www.recaptcha.net/recaptcha/api.js?render={{config('attendize.recaptcha_site')}}"></script>
                     <script>
@@ -86,7 +86,7 @@
                     </script>
                 @endif
                 <div class="form-group ">
-                @if(config('attendize.hcaptcha_site_key'))
+                @if(config('attendize.captcha.hcaptcha_is_on') && config('attendize.hcaptcha_site_key') && ! config('attendize.captcha.recaptcha_is_on'))
                     <script src="https://hcaptcha.com/1/api.js" async defer></script>
                     <input class="btn btn-block btn-success h-captcha" type="submit" value="{{trans('User.sign_up')}}" data-sitekey="{{config('attendize.hcaptcha_site_key')}}" data-callback="onSubmit">
                     <script type="text/javascript">
